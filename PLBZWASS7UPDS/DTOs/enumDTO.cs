b3516280f56b78ec2c6bc3bfc2a0e803f7925ea5
Antonio Arauz Cabello <antonio.arauz@UPDS.edu.bo>
2023-03-24 15:46:59 -0400

    updatePlantillaWSS

--- PLBZWASS7UPDS/DTOs/enumDTO.cs
+++ PLBZWASS7UPDS/DTOs/enumDTO.cs
@@ -14,11 +14,28 @@
     }
     public enum Accept
     {
-        imagen=1,
-        video=2,
-        audio=3,
-        documento=4,
-        all=5
+        imagen = 1,
+        video = 2,
+        audio = 3,
+        documento = 4,
+        all = 5,
+        imagenstatica = 6
 
+    }
+    public enum Dato
+    {
+        imagen = 1,
+        video = 2,
+        audio = 3,
+        documento = 4,
+        gif = 5,
+        otro = 6,
+        undefined = 7,
+
+    }
+    public enum TipoDagAndDrop
+    {
+        Box= 0,
+        Circle = 1,
     }
 }
